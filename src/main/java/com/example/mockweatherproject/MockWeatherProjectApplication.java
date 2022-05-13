d51edc7070bc41b4f19e290d8a106d4e4b604da3
Stephen Mac Donnacha <stephenmcdonagh45@gmail.com>
2022-05-13 11:23:45 +0100

    Initial commit, Spring boot set up

---
+++ src/main/java/com/example/mockweatherproject/MockWeatherProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockweatherproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockWeatherProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockWeatherProjectApplication.class, args);
+	}
+
+}
