c10bf57182c03813f81d37413ca1259619114bc4
Stephen Mac Donnacha <stephenmcdonagh45@gmail.com>
2022-05-14 16:23:09 +0100

    Created class to test Weather Sensor, currently empty

---
+++ src/test/java/com/example/mockweatherproject/WeatherSensorTests.java
@@ -0,0 +1,12 @@
+package com.example.mockweatherproject;
+
+import com.example.mockweatherproject.model.WeatherSensor;
+import org.junit.jupiter.*;
+import org.junit.*;
+
+// Testing class for Weather Sensors
+public class WeatherSensorTests {
+    @Test
+    public void shouldCreateSensor(){
+    }
+}
