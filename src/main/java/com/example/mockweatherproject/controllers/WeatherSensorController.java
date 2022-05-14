9a00c2dc6178befaa45e6eff750138f1cdb00f89
Stephen Mac Donnacha <stephenmcdonagh45@gmail.com>
2022-05-14 15:35:12 +0100

    Added DeleteById Method

--- src/main/java/com/example/mockweatherproject/controllers/WeatherSensorController.java
+++ src/main/java/com/example/mockweatherproject/controllers/WeatherSensorController.java
@@ -3,11 +3,9 @@ package com.example.mockweatherproject.controllers;
 import com.example.mockweatherproject.model.WeatherSensor;
 import com.example.mockweatherproject.repositories.WeatherSensorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.management.Query;
 import java.util.List;
 
 @RestController
@@ -19,10 +17,17 @@ public class WeatherSensorController {
     public List<WeatherSensor> getAllWeatherSensors(){
         return weatherSensorRepository.findAll();
     }
-
-    @PostMapping("/CreateWeatherSensor")
+    
+    @PostMapping("/CreateWeatherSensor") // POST method to create a weather sensor object
     public void createWeatherSensor(@RequestBody WeatherSensor weatherSensor){
         WeatherSensor ws = weatherSensorRepository.insert(weatherSensor);
         System.out.println("New Weather Sensor created");
     }
+
+    @DeleteMapping("/DeleteWeatherSensor/{id}")
+    public void deleteWeatherSensor(@PathVariable("id") WeatherSensor id){
+        this.weatherSensorRepository.delete(id);
+        System.out.println("Deleted Weather sensor");
+    }
+
 }
