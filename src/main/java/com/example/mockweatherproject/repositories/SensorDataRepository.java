0d8a6b6c4605939a7d42e461fc8e6602e9ad8116
Stephen Mac Donnacha <stephenmcdonagh45@gmail.com>
2022-05-15 17:59:21 +0100

    Added method signature to interface

--- src/main/java/com/example/mockweatherproject/repositories/SensorDataRepository.java
+++ src/main/java/com/example/mockweatherproject/repositories/SensorDataRepository.java
@@ -2,10 +2,13 @@ package com.example.mockweatherproject.repositories;
 
 import com.example.mockweatherproject.model.SensorData;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SensorDataRepository extends MongoRepository<SensorData, String> {
+    @Query("{date:'?0'}")
+    SensorData findItemByName(String date);
 }
