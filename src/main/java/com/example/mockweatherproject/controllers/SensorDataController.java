371877cd3f793cb85709c1a38faa4efbe0259176
Stephen Mac Donnacha <stephenmcdonagh45@gmail.com>
2022-05-15 17:57:29 +0100

    Added new API to get data by date

--- src/main/java/com/example/mockweatherproject/controllers/SensorDataController.java
+++ src/main/java/com/example/mockweatherproject/controllers/SensorDataController.java
@@ -1,12 +1,16 @@
 package com.example.mockweatherproject.controllers;
 
 import com.example.mockweatherproject.model.SensorData;
+import com.example.mockweatherproject.model.WeatherSensor;
 import com.example.mockweatherproject.repositories.SensorDataRepository;
 import com.example.mockweatherproject.repositories.WeatherSensorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SensorDataController {
@@ -16,7 +20,21 @@ public class SensorDataController {
     @PostMapping("/createSensorData")
     public void createSensorData(@RequestBody SensorData sensorData){
         SensorData sd = sensorDataRepository.insert(sensorData);
+        System.out.println(sd);
         System.out.println("Sensor Data added");
+    }
+
+    // GET Sensor Data (all)
+    @GetMapping("/getAllData")
+    public List<SensorData> getAllSensorData(){
+        return sensorDataRepository.findAll();
+    }
+
+    // GET Sensor Data by date
+    @GetMapping("/getDataByDate")
+    public void getSensorDataByDate(String date){
+        SensorData sd = sensorDataRepository.findItemByName(date);
+        System.out.println(sd);
     }
 
 }
