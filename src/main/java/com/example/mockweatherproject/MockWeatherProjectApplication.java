af48cf2a53eb2c6f8aafe5927042707553e2afa9
Stephen Mac Donnacha <stephenmcdonagh45@gmail.com>
2022-05-16 11:11:54 +0100

    removed redundant imports

--- src/main/java/com/example/mockweatherproject/MockWeatherProjectApplication.java
+++ src/main/java/com/example/mockweatherproject/MockWeatherProjectApplication.java
@@ -1,10 +1,12 @@
 package com.example.mockweatherproject;
 
+import com.example.mockweatherproject.controllers.SensorDataController;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MockWeatherProjectApplication {
+public class MockWeatherProjectApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(MockWeatherProjectApplication.class, args);
 	}
