0ea6144e861d72bba3a5e02834f3ed30919e5bc4
Stephen Mac Donnacha <stephenmcdonagh45@gmail.com>
2022-05-16 12:14:08 +0100

    Updated with comments for explainability

--- src/test/java/com/example/mockweatherproject/SensorDataTests.java
+++ src/test/java/com/example/mockweatherproject/SensorDataTests.java
@@ -8,11 +8,11 @@ import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-
+// Test class for the SensorData Objects
 @SpringBootTest
 public class SensorDataTests {
     @Autowired
-    private WebApplicationContext webApplicationContext;
+    private WebApplicationContext webApplicationContext; // Used to set up the web context for testing
 
     private DefaultMockMvcBuilder mockMvc;
 
--- src/test/java/com/example/mockweatherproject/WeatherSensorTests.java
+++ src/test/java/com/example/mockweatherproject/WeatherSensorTests.java
@@ -1,8 +1,22 @@
 package com.example.mockweatherproject;
 
-import com.example.mockweatherproject.model.WeatherSensor;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 // Testing class for Weather Sensors
+@SpringBootTest
 public class WeatherSensorTests {
+    @Autowired
+    private WebApplicationContext webApplicationContext; // Used to set up the web context for testing
 
+    private DefaultMockMvcBuilder mockMvc;
+
+    @Before // This should be run before any tests are run
+    public void setup(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext);
+    }
 }
